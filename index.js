97121088d2b21a355d499a8759984fa77ab25735
Benson Njuguna <njugunab655@gmail.com>
2023-03-27 13:05:24 +0300

    Add navigation on click

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import { BookList } from "./modules/booklist.js";
+import { navBtns, bookListSection, renderPage } from "./modules/navigation.js";
 
 const myBookList = new BookList;
 myBookList.setupEventListeners();
+
+navBtns.forEach((each) => {
+  each.addEventListener('click', renderPage);
+});
+
+window.onload = () => {
+  bookListSection.style.display = 'flex';
+  Array.from(navBtns)[0].className = 'btn-active';
+};
