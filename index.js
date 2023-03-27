c4632cfe547aa3dd167b510719f88a6bb4d09754
Benson Njuguna <njugunab655@gmail.com>
2023-03-27 12:56:26 +0300

    Add booklist module

---
+++ index.js
@@ -0,0 +1,4 @@
+import { BookList } from "./modules/booklist.js";
+
+const myBookList = new BookList;
+myBookList.setupEventListeners();
---
+++ modules/booklist.js
@@ -0,0 +1,78 @@
+export class BookList {
+  constructor() {
+    this.form = document.querySelector('#book-form');
+    this.bookListContainer = document.querySelector('#book-list');
+    this.books = [];
+    this.newListBook = {};
+    this.loadBooksFromLocalStorage();
+    this.displayBooks();
+  }
+
+  book(title, author) {
+    this.newListBook.title = title;
+    this.newListBook.author = author;
+    return this.newListBook;
+  }
+
+  loadBooksFromLocalStorage() {
+    const storedBooks = localStorage.getItem('books');
+    if (storedBooks) {
+      this.books = JSON.parse(storedBooks);
+    } else {
+      localStorage.setItem('books', JSON.stringify(this.books));
+    }
+  }
+  
+  displayBooks() {
+    this.bookListContainer.innerHTML = '';
+    this.books.forEach((book, index) => {
+      const li = document.createElement('div');
+      function capitalize(name) {
+        return name.split('').map((each, index) => {
+          if (index === 0) {
+            return each.toUpperCase();
+          }
+          return each;
+        }).join('');
+      }
+      li.className = 'book';
+      
+      const title = capitalize(book.title);
+      const author = capitalize(book.author);
+      
+      li.innerHTML = `"${title}" by ${author}`;
+      const removeBtn = document.createElement('button');
+      removeBtn.className = 'remove';
+      removeBtn.textContent = 'Remove';
+      removeBtn.setAttribute('data-index', index);
+      li.appendChild(removeBtn);
+      this.bookListContainer.appendChild(li);
+    });
+    this.form.reset();
+  }
+  
+  addBook(event) {
+    event.preventDefault();
+    const title = event.target[0].value;
+    const author = event.target[1].value;
+    const newBook = this.book(title, author);
+    this.books.push(newBook);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
+  setupEventListeners() {
+    this.form.addEventListener('submit', this.addBook.bind(this));
+    this.bookListContainer.addEventListener('click', (e) => {
+      if (e.target.className === 'remove') {
+        this.removeBook(e.target.dataset.index);
+      }
+    });
+  }
+}
